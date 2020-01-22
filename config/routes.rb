1c8af1fed16d543aaba1160868cadb6c2320f220
Sinh00 <msoirnthe@gmail.com>
2020-01-22 23:03:52 +0900

    add new & show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # ホーム
-  get "/" => 'home#top'
-  get "about" => 'home#about'
+  get "/" => "home#top"
+  get "about" => "home#about"
 
   # 投稿
-  get "posts" => 'posts#index'
+  get "posts" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts/create"
 end
