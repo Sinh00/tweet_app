22a1e2f2b0355c529af81b0cbfb4a644b2769d42
Sinh00 <msoirnthe@gmail.com>
2020-02-15 00:52:28 +0900

    過去の修正をコミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_064757) do
+ActiveRecord::Schema.define(version: 2020_01_25_151027) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_064757) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "image_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
