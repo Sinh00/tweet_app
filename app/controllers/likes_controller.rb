7b4126aaa33da4326c9a88c326f08aeb9f4cf39e
Sinh00 <msoirnthe@gmail.com>
2021-03-22 22:36:45 +0900

    [add] 投稿の詳細画面にいいね機能をAjaxで実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,12 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
-
-    redirect_to post_path(id: params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   def destroy
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
-
-    redirect_to post_path(id: params[:post_id])
+    @post = Post.find(params[:post_id])
   end
-
 end
